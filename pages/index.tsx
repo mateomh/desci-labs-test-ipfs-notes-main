2d8ce770570ae25212f6bb8ad0f16b275d79fd43
Mateo mojica <mmojicah.84@gmail.com>
2023-08-06 16:12:17 -0500

    Modify the index file to use the created comopnents

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,21 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { BasicIpfsData } from "./api/ipfs";
+import { create } from "ipfs-http-client";
+import { createHelia } from 'helia';
+import { strings } from '@helia/strings';
+import SidePanel from "./components/sidepanel/sidepanel";
+import ContentSection from "./components/contentsection/contentsection";
+import WellMessage from "./components/wellmessage/wellmessage";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
-  const [result, setResult] = useState<BasicIpfsData | null>(null);
-
-  const [note, setNote] = useState<BasicIpfsData | null>(null);
-  const [txt, setTxt] = useState("");
-
-  const handleLoad = async () => {
-    setLoading(true);
-
-    const { data } = await axios.get("/api/ipfs");
-    setResult(data);
-    setLoading(false);
-  };
-
-  // avoiding ternary operators for classes
-  function classNames(...classes: any) {
-    return classes.filter(Boolean).join(" ");
-  }
+  const [notes, setNotes] = useState<BasicIpfsData[]>([]);
+  const [displayWell, setDisplayWell] = useState<boolean>(false);
+  const [wellText, setWellText] = useState<string>("");
 
   return (
     <>
@@ -35,25 +26,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="flex flex-col gap-3">
-          <div className="text-3xl font-bold underline">IPFS Notes</div>
-          {!!result ? (
-            <div className="flex flex-col">
-              <span>Content: {result.content}</span>
-              <span>CID: {result.cid}</span>
-            </div>
-          ) : null}
-          <div>
-            <button
-              onClick={handleLoad}
-              className={classNames(
-                "bg-slate-300 hover:bg-slate-500 text-black rounded-md p-2 drop-shadow-md w-32",
-                loading ? "animate-pulse" : ""
-              )}
-            >
-              {loading ? "Loading..." : "Retrieve Data"}
-            </button>
-          </div>
+
+        <div className="flex">
+          <WellMessage 
+            text={wellText} 
+            display={displayWell} 
+            setDisplayWell={setDisplayWell}
+          />
+          <SidePanel 
+            setWellText={setWellText} 
+            setDisplayWell={setDisplayWell}
+            setNotes={setNotes}
+          />
+          <ContentSection 
+            notes={notes}
+          />
         </div>
       </main>
     </>
