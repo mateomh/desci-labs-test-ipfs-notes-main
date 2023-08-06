d4b31c35063ea514064786b152789a481aebcfeb
Mateo mojica <mmojicah.84@gmail.com>
2023-08-06 16:13:01 -0500

    Modify the endpoint to add a note to the ipfs

--- pages/api/ipfs.ts
+++ pages/api/ipfs.ts
@@ -1,6 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-
+import { createHelia } from "helia";
+import { strings } from "@helia/strings";
+import { CID, Version } from "multiformats/cid";
+import { base64 } from "multiformats/bases/base64"
 import { create } from "ipfs-http-client";
 
 // arbitrary response format
@@ -15,23 +18,31 @@ export default async function handler(
 ) {
   if (req.method === "POST") {
     // Process a POST request
+    submitNote(req, res);
   } else {
     // Handle any other HTTP method
     retrieveData(req, res);
   }
 }
 
+const submitNote = async (
+  req: NextApiRequest,
+  res: NextApiResponse<BasicIpfsData>
+) => {
+  const helia = await createHelia();
+  const s = strings(helia);
+  try {
+    const cid = await s.add(req.body.note)
+    console.log("ADDRESS: ", cid);
+    res.status(200).json({ cid: cid.toString() , content: await s.get(cid) });
+  } catch (error) {
+    res.status(400);
+  }
+};
+
 const retrieveData = async (
   req: NextApiRequest,
   res: NextApiResponse<BasicIpfsData>
 ) => {
-  // connect to the default API address http://localhost:5001
-  const client = create();
-  // call Core API methods
-  const string = "Hello world!";
-  const data = await client.add(string);
-
-  console.log(data);
-
-  res.status(200).json({ cid: data.path, content: string });
+  res.status(400);
 };
